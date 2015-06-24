58f259774e08e4c63938cf2fd7f3196994bf1026
Luke Diamand <luke@diamand.org>
2015-06-24 20:37:06 +0100

    mmal-leak: build a better test case
    Builds a test case that runs on an x86-64 machine (so can use
    valgrind) and uses the "null sink" component rather than a
    real videocore component, to avoid getting confused by the
    behaviour of the videocore.
    
    Signed-off-by: Luke Diamand <luke@diamand.org>

--- mmal-leak.c
+++ mmal-leak.c
@@ -12,115 +12,47 @@
 
 #include <interface/mmal/mmal.h>
 #include <interface/mmal/util/mmal_util.h>
+#include "interface/mmal/util/mmal_default_components.h"
 
 #define MMAL_COMPONENT_DEFAULT_DEINTERLACE "vc.ril.image_fx"
 
-static void output_port_cb(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
-	printf("Output port cb\n");
-}
-
 int main()
 {
-	MMAL_COMPONENT_T *image_fx = NULL;
+	MMAL_COMPONENT_T *comp = NULL;
 	MMAL_POOL_T *pool = NULL;
 	MMAL_STATUS_T status;
-	MMAL_PORT_T *output;
-	MMAL_PORT_T *input;
+	MMAL_PORT_T *port;
 	unsigned i;
 
-	status = mmal_component_create(MMAL_COMPONENT_DEFAULT_DEINTERLACE, &image_fx);
+	status = mmal_component_create("null_sink", &comp);
 	if (status != MMAL_SUCCESS) {
 		printf("Could not create image_fx component %s (status=%"PRIx32" %s)\n",
 				MMAL_COMPONENT_DEFAULT_DEINTERLACE, status,
 				mmal_status_to_string(status));
 		return EXIT_FAILURE;
 	}
+    assert(comp);
 
-	output = image_fx->output[0];
-	input = image_fx->input[0];
+	port = comp->input[0];
 
-	input->format->encoding = MMAL_ENCODING_OPAQUE;
-	status = mmal_port_format_commit(input);
-	if (status != MMAL_SUCCESS) {
-		printf("Failed to commit format for input port %s (status=%"PRIx32" %s)",
-			input->name, status, mmal_status_to_string(status));
-		return EXIT_FAILURE;
-	}
-
-	status = mmal_port_enable(input, output_port_cb);
-	if (status != MMAL_SUCCESS) {
-		printf("Could not enable output port on %s (status=%"PRIx32" %s)\n",
-				MMAL_COMPONENT_DEFAULT_DEINTERLACE, status,
-				mmal_status_to_string(status));
-		return EXIT_FAILURE;
-	}
-
-	mmal_format_full_copy(output->format, input->format);
-	status = mmal_port_format_commit(output);
-	if (status != MMAL_SUCCESS) {
-		printf("Failed to commit format for input port %s (status=%"PRIx32" %s)",
-			output->name, status, mmal_status_to_string(status));
-		return EXIT_FAILURE;
-	}
-
-	output->buffer_size = output->buffer_size_recommended;
-	output->buffer_num = output->buffer_num_recommended;
-	status = mmal_port_enable(output, output_port_cb);
-	if (status != MMAL_SUCCESS) {
-		printf("Could not enable output port on %s (status=%"PRIx32" %s)\n",
-				MMAL_COMPONENT_DEFAULT_DEINTERLACE, status,
-				mmal_status_to_string(status));
-		return EXIT_FAILURE;
-	}
-
-	status = mmal_component_enable(image_fx);
-	if (status != MMAL_SUCCESS) {
-		printf("Could not enable component %s (status=%"PRIx32" %s)\n",
-				MMAL_COMPONENT_DEFAULT_DEINTERLACE, status,
-				mmal_status_to_string(status));
-		return EXIT_FAILURE;
-	}
-
-	printf("Components initialised...\n");
-	usleep(10 * 1000 * 1000);
-
-	printf("Allocate and free 10000 pools using default allocator...\n");
-	for(i = 0; i < 10000; i++) {
-		printf("Create pool %u, size %dx%d\n", i,
-				output->buffer_num,
-				output->buffer_size);
+    int count = 10;
+	printf("Allocate and free %d pools using port payload allocator...\n", count);
+	for(i = 0; i < count; i++) {
 		if (pool)
 			mmal_pool_destroy(pool);
 
-		pool = mmal_pool_create(output->buffer_num,
-				output->buffer_size);
-		if (!pool) {
-			printf("Could not create pool\n");
-			return EXIT_FAILURE;
-		}
-	}
-	printf("Done allocating and freeing 10000 pools using default allocator...\n");
-	usleep(10 * 1000 * 1000);
-
-	printf("Allocate and free 10000 pools using port payload allocator...\n");
-	for(i = 0; i < 10000; i++) {
-		printf("Create pool %u, size %dx%d\n", i,
-				output->buffer_num,
-				output->buffer_size);
-		if (pool)
-			mmal_pool_destroy(pool);
-
-		pool = mmal_pool_create_with_allocator(output->buffer_num,
-				output->buffer_size, output,
+		pool = mmal_pool_create_with_allocator(10, 256, port,
 				(mmal_pool_allocator_alloc_t)mmal_port_payload_alloc,
 				(mmal_pool_allocator_free_t)mmal_port_payload_free);
 		if (!pool) {
-			printf("Could not create pool\n");
+			printf("Could not create pool");
 			return EXIT_FAILURE;
 		}
 	}
-	printf("Done allocating and freeing 10000 pools using port payload allocator...\n");
-	usleep(10 * 1000 * 1000);
+    mmal_pool_destroy(pool);
+	printf("Done allocating and freeing %d pools using port payload allocator...\n", count);
+    mmal_component_destroy(comp);
+    printf("Destroyed component\n");
 
 	return 0;
 }
